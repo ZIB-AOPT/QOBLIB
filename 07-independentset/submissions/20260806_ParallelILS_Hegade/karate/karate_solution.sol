2330c46636703f46cee4902ec70439df194ba505
Narendra Hegade <narendrahegade5@gmail.com>
2026-08-06 22:26:16 +0200

    Cooperative parallel ILS solutions for MIS

---
+++ 07-independentset/submissions/20260806_ParallelILS_Hegade/karate/karate_solution.sol
@@ -0,0 +1,22 @@
+# Cooperative parallel ILS solution for karate
+# Objective value = 20
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+19
+20
+21
+22
+23
+26
+28
+29
+30
---
+++ 07-independentset/submissions/20260806_ParallelILS_Hegade/sloane_1dc_64/sloane_1dc_64_solution.sol
@@ -0,0 +1,12 @@
+# Cooperative parallel ILS solution for sloane_1dc_64
+# Objective value = 10
+1
+4
+13
+16
+19
+46
+49
+52
+61
+64
